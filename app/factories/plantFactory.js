25023e216a927e880b5204be6e13424baa31c88f
Ben Conner <ben.conner40@gmail.com>
2017-05-13 23:37:25 -0500

    factory for getting plant info from db

---
+++ app/factories/plantFactory.js
@@ -0,0 +1,15 @@
+angular.module('Smart_Gardener')
+.factory('plantFactory', function($http){
+  return{
+    getPlants : ()=>{
+      return $http
+      .get(`http://localhost:3000/api/v1/plants`)
+      .then((res)=>{
+          return res
+      })
+      .catch((res)=>{
+          console.error(res);
+      })
+    }
+  }
+})
