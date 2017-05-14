ab1b8bdd549c78aceace284ea4eab66e125cd452
Ben Conner <ben.conner40@gmail.com>
2017-05-13 23:36:02 -0500

    scope for plants response from db

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,4 +1,8 @@
 angular.module('Smart_Gardener')
-.controller('HomeCtrl', function($scope){
-  $scope.hi = "howdy"
+.controller('HomeCtrl', function($scope, plantFactory){
+  plantFactory.getPlants()
+  .then((res)=>{
+    $scope.plants = res.data
+    console.log(res);
+  })
 })
