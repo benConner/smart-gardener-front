dafcf7d80c60f856e98d8b3ad4417c4e78e1bb4a
Ben Conner <ben.conner40@gmail.com>
2017-05-15 10:45:02 -0500

    gardens being pulled and displayed

--- app/controllers/gardensCtrl.js
+++ app/controllers/gardensCtrl.js
@@ -1,4 +1,8 @@
 angular.module('Smart_Gardener')
-.controller('GardensCtrl', function($scope) {
-  $scope.hi = "gardens yo"
+.controller('GardensCtrl', function($scope, gardenFactory) {
+  gardenFactory.getGardens()
+  .then((res)=>{
+    $scope.gardens = res.data
+    console.log($scope.gardens);
+  })
 })
