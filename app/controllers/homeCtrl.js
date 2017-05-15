0f1dd36dc05e95bd9deba4acfede0a51df5b1675
Ben Conner <ben.conner40@gmail.com>
2017-05-15 10:48:44 -0500

    homeCtrl function that selects a plant by id and takes you to the url for it on click

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,8 +1,12 @@
 angular.module('Smart_Gardener')
-.controller('HomeCtrl', function($scope, plantFactory){
+.controller('HomeCtrl', function($scope,$location, plantFactory){
   plantFactory.getPlants()
   .then((res)=>{
     $scope.plants = res.data
     console.log(res);
   })
+
+  $scope.plantById = (id) =>{
+    $location.url(`/plant/${id}`)
+  }
 })
