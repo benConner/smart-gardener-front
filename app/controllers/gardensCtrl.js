bc0afe7f3c8fcf5f47b5c85c6d485505bfe68628
Ben Conner <ben.conner40@gmail.com>
2017-05-17 15:26:00 -0500

    deleteing garden

--- app/controllers/gardensCtrl.js
+++ app/controllers/gardensCtrl.js
@@ -1,5 +1,5 @@
 angular.module('Smart_Gardener')
-.controller('GardensCtrl', function($scope, $location, gardenFactory) {
+.controller('GardensCtrl', function($scope, $location, $route, gardenFactory) {
   gardenFactory.getGardens()
   .then((res)=>{
     $scope.gardens = res.data
@@ -7,5 +7,12 @@ angular.module('Smart_Gardener')
   })
   $scope.getGardenId = (gardenId) => {
     $location.url(`/garden/${gardenId}`)
+  }
+  $scope.delete = (id)=>{
+    gardenFactory.deleteGarden(id)
+    .then((res)=>{
+      $route.reload();
+      console.log(res);
+    })
   }
 })
