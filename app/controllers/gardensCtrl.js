f5cbffc1fd171cbd8dba4b29f1a22901cf93401e
Ben Conner <ben.conner40@gmail.com>
2017-05-18 15:04:40 -0500

    calling function to render the page data

--- app/controllers/gardensCtrl.js
+++ app/controllers/gardensCtrl.js
@@ -1,17 +1,22 @@
 angular.module('Smart_Gardener')
 .controller('GardensCtrl', function($scope, $location, $route, gardenFactory) {
-  gardenFactory.getGardens()
-  .then((res)=>{
-    $scope.gardens = res.data
-    console.log($scope.gardens);
-  })
+
+  (getGardenNames = () => {
+    gardenFactory.getGardens()
+    .then((res)=>{
+      $scope.gardens = res.data
+      console.log($scope.gardens);
+    })
+  })()
+
   $scope.getGardenId = (gardenId) => {
     $location.url(`/garden/${gardenId}`)
   }
+
   $scope.delete = (id)=>{
     gardenFactory.deleteGarden(id)
     .then((res)=>{
-      $route.reload();
+      getGardenNames();
       console.log(res);
     })
   }
