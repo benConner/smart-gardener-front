0e61d72c17a4d614209470ac65a22d1b4dc41ca6
Ben Conner <ben.conner40@gmail.com>
2017-05-18 15:03:01 -0500

    code clean up deleting console.logs and unused code

--- app/controllers/addGardenCtrl.js
+++ app/controllers/addGardenCtrl.js
@@ -2,10 +2,8 @@ angular.module('Smart_Gardener')
 .controller('AddGardenCtrl', function($scope, $location, gardenFactory){
   $scope.addGarden = (gardenObj, idArray) => {
     gardenObj.plantsId = idArray
-    console.log(gardenObj);
     gardenFactory.addGarden(gardenObj)
     .then((res)=>{
-      console.log(res);
       $location.url('/gardens')
     })
   }
