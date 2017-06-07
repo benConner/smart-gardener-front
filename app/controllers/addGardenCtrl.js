52f9d611e72b0c2cf6159f6f44d18072ffd41e8b
Ben Conner <ben.conner40@gmail.com>
2017-06-07 17:26:38 -0500

    addGarden comment for readability

--- app/controllers/addGardenCtrl.js
+++ app/controllers/addGardenCtrl.js
@@ -2,6 +2,7 @@ angular.module('Smart_Gardener')
 .controller('AddGardenCtrl', function($scope, $location, gardenFactory){
   $scope.addGarden = (gardenObj, idArray) => {
     gardenObj.plantsId = idArray
+    //function on click to add garden to smart gardener DB and redirect to gadrens page
     gardenFactory.addGarden(gardenObj)
     .then((res)=>{
       $location.url('/gardens')
