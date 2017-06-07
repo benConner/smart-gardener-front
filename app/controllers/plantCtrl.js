59e5e1fae38e2f163e0d01da8b04c8b36825fe31
Ben Conner <ben.conner40@gmail.com>
2017-06-07 17:32:25 -0500

    comments for readability

--- app/controllers/plantCtrl.js
+++ app/controllers/plantCtrl.js
@@ -1,6 +1,7 @@
 angular.module('Smart_Gardener')
 .controller('PlantCtrl', function($scope, $routeParams, plantFactory) {
 
+  //shows plant information by Id
   plantFactory.getPlantById($routeParams.id)
   .then((res)=>{
     console.log($routeParams.id);
