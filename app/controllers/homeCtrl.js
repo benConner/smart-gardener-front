de3b2045f9cd1c31a5e6bd2cb092f7a6071510e5
Ben Conner <ben.conner40@gmail.com>
2017-06-07 17:27:33 -0500

    homeCrtl comments for readability

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,11 +1,12 @@
 angular.module('Smart_Gardener')
 .controller('HomeCtrl', function($scope,$location, plantFactory){
+  //loads in plant information
   plantFactory.getPlants()
   .then((res)=>{
     $scope.plants = res.data
     console.log(res);
   })
-
+  //pass plant id as a route param
   $scope.plantById = (id) =>{
     $location.url(`/plant/${id}`)
   }
