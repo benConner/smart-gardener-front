083cac242c0e391dc4a0e5e3f4442e4f10d00944
Ben Conner <ben.conner40@gmail.com>
2017-06-07 17:27:04 -0500

    garden comments for readability

--- app/controllers/gardensCtrl.js
+++ app/controllers/gardensCtrl.js
@@ -1,23 +1,24 @@
 angular.module('Smart_Gardener')
 .controller('GardensCtrl', function($scope, $location, $route, gardenFactory) {
 
+  //self excuting funtion to load in garden names from smart gardener DB
   (getGardenNames = () => {
     gardenFactory.getGardens()
     .then((res)=>{
       $scope.gardens = res.data
-      console.log($scope.gardens);
     })
   })()
 
+  // when garden is selected passes the garden id as a route param
   $scope.getGardenId = (gardenId) => {
     $location.url(`/garden/${gardenId}`)
   }
 
+  //function to delete garden on x click
   $scope.delete = (id)=>{
     gardenFactory.deleteGarden(id)
     .then((res)=>{
       getGardenNames();
-      console.log(res);
     })
   }
 })
